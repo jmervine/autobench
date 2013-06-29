4e231d73522501aaec1135c2a14373a42fce0313
Josh Mervine <jmervine@yp.com>
2013-06-29 00:01:39 -0700

    Fixing tagging issues.

--- examples/minitest-spec.rb
+++ examples/minitest-spec.rb
@@ -2,31 +2,31 @@ require "minitest/autorun"
 require "autobench"
 
 describe "when I run autobench" do
-	before do
-		@autobench ||= Autobench.new("./config/mervine.yml")
-	end
+  before do
+    @autobench ||= Autobench.new("./config/mervine.yml")
+  end
 
-	describe "render" do
-		it "passes thresholds" do
+  describe "render" do
+    it "passes thresholds" do
       @autobench.render.benchmark
       assert @autobench.render.passed?,
           "Passed:\n#{@autobench.render.successes}\nFailed:\n#{@autobench.render.failures}"
-		end
-	end
+    end
+  end
 
-	describe "client" do
-		it "passes thresholds" do
+  describe "client" do
+    it "passes thresholds" do
       @autobench.client.benchmark
       assert @autobench.client.passed?,
           "Passed:\n#{@autobench.client.successes}\nFailed:\n#{@autobench.client.failures}"
-		end
-	end
+    end
+  end
 
-	describe "yslow" do
-		it "passes thresholds" do
+  describe "yslow" do
+    it "passes thresholds" do
       @autobench.yslow.benchmark
       assert @autobench.yslow.passed?,
           "Passed:\n#{@autobench.yslow.successes}\nFailed:\n#{@autobench.yslow.failures}"
-		end
-	end
+    end
+  end
 end
--- examples/rspec.rb
+++ examples/rspec.rb
@@ -5,34 +5,34 @@
 require 'autobench'
 
 describe "when I run autobench" do
-	before do
-		@autobench ||= Autobench.new("./config/mervine.yml")
-	end
+  before do
+    @autobench ||= Autobench.new("./config/mervine.yml")
+  end
 
-	describe "render" do
-		it "passes thresholds" do
+  describe "render" do
+    it "passes thresholds" do
       @autobench.render.benchmark
       @autobench.render.failures.should == "none"
 
       # note:
-      # 
+      #
       # testing "failures" over "passed?" because it will
       # provide more verbose output on what went wrong
 
-		end
-	end
+    end
+  end
 
-	describe "client" do
-		it "passes thresholds" do
+  describe "client" do
+    it "passes thresholds" do
       @autobench.client.benchmark
       @autobench.client.failures.should == "none"
-		end
-	end
+    end
+  end
 
-	describe "yslow" do
-		it "passes thresholds" do
+  describe "yslow" do
+    it "passes thresholds" do
       @autobench.yslow.benchmark
       @autobench.yslow.failures.should == "none"
-		end
-	end
+    end
+  end
 end
